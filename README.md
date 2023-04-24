9aee150bf8a29ebef89dbacf7fa957dff1c2167f
Roma Yaskevich <75991248+ehtok@users.noreply.github.com>
2023-04-24 13:51:17 +0300

    Creating a project structure. Adding pom file and dependencies, application.yml, changelog.xml and file for start application

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# searcher
+# Searcher
+# **List of environment variables**
+DATABASE_URL - Database URL. (Example: "jdbc:mysql://localhost:3306/****" or "jdbc:mysql://192.168.205.200:3306/****")
+DATABASE_USERNAME - Database username.
+DATABASE_PASSWORD - Database password.
---
+++ src/main/java/by/searcher/main/ApplicationSearcher.java
@@ -0,0 +1,11 @@
+package by.searcher.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSearcher {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationSearcher.class, args);
+    }
+}
